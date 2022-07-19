1f02e6501be717efc3720fd54565002e30a74071
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-07-19 20:11:29 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const calculator = document.getElementById('calcualtor');
+const keys = document.querySelector('.calculator--keys');
+const 
+keys.addEventListener('click', e => {
+  if (e.target.matches('button'){
+    alert('I was clicked')
+  })
+})
